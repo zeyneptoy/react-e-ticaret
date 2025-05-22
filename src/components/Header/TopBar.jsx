c109414ba7185d9ed8ce13009550bc79f9f8fb28
Zeynep Toy Demir <33411499+zeyneptoy@users.noreply.github.com>
2025-05-22 18:09:22 +0300

    TopBar ilk animasyon eklendi.

--- src/components/Header/TopBar.jsx
+++ src/components/Header/TopBar.jsx
@@ -1,24 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { motion, AnimatePresence } from "motion/react"
+
 
 const TopBar = () => {
+
+    const [dillerAcikMi, setDillerAcikMi] = useState(false);
+
+    const dilleriGoster = () => {
+        setDillerAcikMi(!dillerAcikMi);
+        console.log(dillerAcikMi);
+        // Burada dilleri gösterme işlemi yapılacak 
+    }
+    const paralariGoster = () => {
+        console.log("Paraları göster");
+    }
+
     return (
-        <div className=' flex justify-between items-center p-3 shadow px-6'>
+        <div className=' flex justify-between items-center  shadow px-6'>
             <div className='flex justify-start items-center p2 gap-5 text-sm '>
                 <a href="/">Hesap</a>
                 <a href="/">Sipariş Takip</a>
                 <a href="/">Destek</a>
             </div>
-            <div className=' flex justify-start items-center p-2 gap-5 text-sm'>
-                <a className='flex justify-center items-center gap-1' href="/">
-                    <img src="/img/turkiye-icon.png" alt="" className='size-6' />Türkiye
+            <div className=' flex justify-start items-center p-2.5 gap-5 text-sm  select-none relative'>
+                <div onClick={dilleriGoster} className='flex justify-center  cursor-pointer items-center gap-1' href="/">
+                    <img src="/img/turkiye-icon.png" alt="" className='size-6' />Türkçe
                     <img src="/img/arrow-down-icon.png" alt="" className='size-3' />
-                </a>
-                <a className='flex justify-center items-center gap-1' href="/">
+                    <AnimatePresence>
+                        {dillerAcikMi && (
+                            <motion.div
+                                initial={{ scale: 0, opacity: 0 }}
+                                animate={{ scale: 1, opacity: 1 }}
+                                exit={{ scale: 0, opacity: 0 }}
+                                className='absolute bg-amber-300 p-10 top-full left-0'>
+                                Diller
+                            </motion.div>)}
+                    </AnimatePresence>
+                </div>
+                <div onClick={paralariGoster} className='flex justify-center  cursor-pointer items-center gap-1' href="/">
                     <img src="/img/currency-icon.png" alt="" className='size-6' />
                     TRY
                     <img src="/img/arrow-down-icon.png" alt="" className='size-3' />
 
-                </a>
+                </div>
             </div>
         </div>
     )
